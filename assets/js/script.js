1eed3825412119d5f1324274ace32a8d45fd5e4d
Rush <kriscookies20@gmail.com>
2022-03-27 18:16:28 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,4 @@
 
-
-
-
-
-//   console.log(askPrompt);
-// //  make prompts
-//   var askPrompt = parseInt(window.prompt('How many characters would you like your password to be? Please choose a number between 8 to 128.'));
-//     //   - PS charLength = 8 - 128 --> validate prompt answer
-//     // returns false until a number from 8 to 128 is selected.
-//     if (askPrompt < 8 || askPrompt > 128 || isNaN(askPrompt)) {
-//       window.prompt('Please type a number value between 8 - 128.');
-//       return "";
-//     }
-//     // if true ask if user wants to add lowercase characters. **confirm makes it a Boolean (True or False)**
-//     if (window.confirm ('Click ok to confirm that you would like to include lowercase characters.')); {
-//     // **concat method joins strings**
-//       charactersArry = charactersArry + lowerCharacters * Math.random;
-//     }
-//     // if true ask if user wants to add uppercase characters.
-//     if (window.confirm ('Click ok to confirm that you would like to include uppercase characters')); {
-//       charactersArry = charactersArry + upperCharacters * Math.random;
-//     }
-  
-//     // if true ask if user wants to add numeric characters.
-//     if (window.confirm ('Click to confirm that you would like to include numeric characters.')); {
-//       charactersArry = charactersArry + numeric * Math.random;
-//     }
-
-//     // if true ask if user wants to add special characters.
-//     if (window.confirm ('Click to confirm that you would like to include special characters.')); {
-
-//       charactersArry = charactersArry + specialCharacters * Math.random;
-//     }
-
-//     for (var i = 0; i < charLength; i++) {
-//     var randomCharacters = Math.floor(Math.random() * charactersArry.length);
-//       password += charactersArry(random, random + 1);
-//     }
-      
-// //   --> input should be validated (validated with Booleans above)
-// //   --> generate PS
-
-// //   - display PS to pg
-//   return (password);
-// }
-
 // Assignment code here
 //   - need lowercase, uppercase, numeric, &/ special characters (variables and an array to list them easier)
 var charactersArry = [];
@@ -55,17 +9,18 @@ var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I','J', 'K', 'L',
 var numeric = ['0', '1', '2', '3', '4', '5','6', '7', '8','9'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^','&', '*', '(', ')', '-', '_', '=', '+','[', '{',']', '}',';', ':', ',', '<','.', '>','?', '/',];
 
-
+// function to have prompts to choose from, from user once the generate PS is clicked
 function promptCalls() {
-charactersArry = [];
+charactersArry = []; //this is empty and allows a restart
 
 charLength = parseInt(prompt("How many characters would you like your password to be? Please choose a number between 8 to 128."));
-
+    //isNaN = is not a number
     if(isNaN(charLength) || charLength < 8 || charLength > 128) {
       alert("Please type a number value between 8 - 128.");
       return false;
     }
-
+    // this if statement and the if statements below are booleans (true/false) & do not need an else
+    // concat used to 
     if (confirm('Click ok to confirm that you would like to include lowercase characters.')) {
       charactersArry = charactersArry.concat(lowerCharacters);
     }
@@ -88,33 +43,35 @@ charLength = parseInt(prompt("How many characters would you like your password t
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
+// This is saying, when i click on the generate password button, then writepassword is referenced
 generateBtn.addEventListener("click", writePassword);
-
 
 // Write password to the #password input
 function writePassword() {
 
-  var truePrompt = promptCalls(); // calling promptCalls & saying if it's true to follow below code
-  var passwordText = document.querySelector("#password");
+  var truePrompt = promptCalls(); // calling promptCalls & saying if it's true to follow below code, if not follow the else code
+  var passwordText = document.querySelector("#password"); // passwordText needs to 
     if (truePrompt) {
   //generatePassword is undefined so i need to define it (defined with function below)
   var password = generatePassword();
   
   // value is being stored to generatePassword
   passwordText.value = password;
-    } else {
+    } else { // if promptCall is false
       passwordText.value = "";
     }
 }
 
-//need value for generatePassword
+//need value for generatePassword (this is why a generatePassword function is created)
 //need to return the generated variable 
  function generatePassword() {
-
-   var passwordGen = "";
-   for (var i = 0; i < charLength; i++) { // this for loops generates random PS
-     var randomPassChar = Math.floor(Math.random() * charactersArry.length);
-      passwordGen = passwordGen + charactersArry[randomPassChar];
+   
+   var passwordGen = ""; // starts w/ empty string to allow it to generate arrays randomly
+   for (var i = 0; i < charLength; i++) { // this for loop generates random PS length
+     var randomPassChar = Math.floor(Math.random() * charactersArry.length); // to get random element from your array
+      passwordGen = passwordGen + charactersArry[randomPassChar]; // to get a random password character from array, added to PasswordGen, and stored into passwordGen
    }
+   // This generates the new paswword after all prompts are answered
+   // value is returned to var password = generatePassword();
    return passwordGen;
  }
